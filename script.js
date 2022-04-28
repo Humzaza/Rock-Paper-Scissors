7f4f0183cfa064ebaeb6883edfc04cfaccc6868c
Humzaza <humzaaamir61@gmail.com>
2022-04-28 18:28:41 -0400

    restarted rps

---
+++ script.js
@@ -0,0 +1,81 @@
+let playerInput ;// prompt("Please enter: rock, paper or scissors");
+let computerOutput = computerPlay();
+let currentPlayerscore = 0;
+let currentComputerScore = 0;
+
+function computerPlay() {
+    let currentPlay = Math.floor(Math.random() * 3);
+    if(currentPlay == 0) {
+        return "rock";
+    }
+    else if (currentPlay == 1) { 
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    while (playerSelection != "rock" || playerSelection != "paper" || playerSelection != "scissors") {
+        if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
+            break;
+        }
+        playerSelection = prompt("Please enter: rock, paper or scissors");
+    }
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection + " " + computerSelection);
+    if(playerSelection == computerSelection) {
+        scoreTracker(1, 1);
+        return `Tie`;
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") {
+        scoreTracker(1, 0);
+        return `You Win! ${playerSelection} beats ${computerOutput}`;
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        scoreTracker(0, 1);
+        return `You Lose! ${computerOutput} beats ${playerSelection}`;
+    }
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        scoreTracker(1, 0);
+        return `You Win! ${playerSelection} beats ${computerOutput}`;
+    }
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        scoreTracker(0, 1);
+        return `You Lose! ${computerOutput} beats ${playerSelection}`;
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
+        scoreTracker(1, 0);
+        return `You Win! ${playerSelection} beats ${computerOutput}`;
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
+        scoreTracker(0, 1);
+        return `You Lose! ${computerOutput} beats ${playerSelection}`;
+    }
+}
+function game () {
+    for (let i = 0; i < 5; i++) {
+        playerInput = prompt("Please enter: rock, paper or scissors");
+        console.log(playRound(playerInput, computerOutput));
+        computerOutput = computerPlay();
+    }
+    console.log(currentPlayerscore+" "+currentComputerScore);
+    
+    if(currentPlayerscore > currentComputerScore) {
+        return 'You Win!';
+    }
+    else if (currentPlayerscore == currentComputerScore) {
+        return 'Its a Tie';
+    }
+    else{
+        return 'Computer Won!';
+    }
+}
+
+function scoreTracker (playerScore, computerScore) {
+    currentPlayerscore = playerScore + currentPlayerscore;
+    currentComputerScore = computerScore + currentComputerScore;
+}
+console.log(game());
